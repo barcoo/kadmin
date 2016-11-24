f690526fee581430a7aa3df8369dfbca3a700d0b
Nicolas Pepin-Perreault <pepin-perreault@checkitmobile.com>
2016-11-24 14:15:14 +0100

    tentative fix to check if TravisCI has autoloading problems

--- test/components/form_test.rb
+++ test/components/form_test.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
 require 'test_helper'
 
 module Kadmin
+  # NOTE: Uses PeopleForm from the dummy app for testing
   class FormTest < ActiveSupport::TestCase
-    
+
   end
 end
