3647f3bb92a534af44e6fdd89839e6930a143b32
Nicolas Pepin-Perreault <pepin-perreault@checkitmobile.com>
2016-11-24 14:28:06 +0100

    start form tests

--- test/components/form_test.rb
+++ test/components/form_test.rb
@@ -2,8 +2,16 @@
 require 'test_helper'
 
 module Kadmin
-  # NOTE: Uses PeopleForm from the dummy app for testing
+  # NOTE: Uses the Person model from the dummy app for testing
   class FormTest < ActiveSupport::TestCase
+    def test_initialize
+      person = Person.create(first_name: 'first', last_name: 'last', gender: 'm', date_of_birth: 2.years.ago)
+      form = Kadmin::Form.new(person)
 
+      assert_equal person, form.model
+      assert_empty form.errors
+      assert form.valid?
+      assert_equal person, form.to_model
+    end
   end
 end
