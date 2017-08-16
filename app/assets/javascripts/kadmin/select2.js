183a648f551e2710e8849a266a5b59a921b990bc
Nicolas Pépin-Perreault <nicolas.pepin-perreault@offerista.com>
2017-08-16 13:17:06 +0200

    linting

--- app/assets/javascripts/kadmin/select2.js
+++ app/assets/javascripts/kadmin/select2.js
@@ -1,19 +1,20 @@
 //= require select2
-$(document).ready(function() {
-  $.fn.select2.defaults.set("theme", "bootstrap");
-  $.fn.select2.defaults.set("ajax", {
+/* global jQuery */
+jQuery(document).ready(function() {
+  jQuery.fn.select2.defaults.set("theme", "bootstrap");
+  jQuery.fn.select2.defaults.set("ajax", {
     cache: true,
     dataType: "json",
     delay: 300,
     data: transformRequest,
     processResults: transformResponse
   });
-  $.fn.select2.defaults.set("allowClear", true);
+  jQuery.fn.select2.defaults.set("allowClear", true);
 
   function transformRequest(params) {
     var page = params.page || 0;
-    var page_size = this.data('kadmin--page-size') || 10;
-    var filter_param = this.data('kadmin--filter-param') || null;
+    var page_size = this.data("kadmin--page-size") || 10;
+    var filter_param = this.data("kadmin--filter-param") || null;
     var options = {
       page_offset: page * page_size,
       page_size: page_size
@@ -29,11 +30,11 @@ $(document).ready(function() {
   function transformResponse(data, params) {
     var items = data.data || data.items;
     var options = this.options.options.kadmin || {}; // weird but it is what it is
-    var displayProperty = options.displayProperty || 'text';
-    var valueProperty = options.valueProperty || 'id';
+    var displayProperty = options.displayProperty || "text";
+    var valueProperty = options.valueProperty || "id";
 
     var results = [];
-    $(items).each(function(index, item) {
+    jQuery(items).each(function(index, item) {
       results.push({ text: item[displayProperty], id: item[valueProperty] });
     });
 
@@ -45,15 +46,15 @@ $(document).ready(function() {
 
   function optionsForSelect2(element) {
     return {
-      minimumInputLength: $(element).data('kadmin--minimum-input-length') || 2,
+      minimumInputLength: jQuery(element).data("kadmin--minimum-input-length") || 2,
     }
   }
 
   // Need to delay a bit otherwise we have issues
   // TODO: Figure out why this happens?
   setTimeout(function() {
-    $('select.kadmin-select2').each(function() {
-      $(this).select2(optionsForSelect2(this));
+    jQuery("select.kadmin-select2").each(function() {
+      jQuery(this).select2(optionsForSelect2(this));
     });
   }, 300);
 
