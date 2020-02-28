758d87e3890e057591f8be7dfa4f2eb044e75709
Teymour Taghavi <teymour.taghavi@offerista.com>
2020-02-28 16:08:29 +0100

    refactor Kadmin.BootstrapHelper.create_custom_label

--- app/helpers/kadmin/bootstrap_helper.rb
+++ app/helpers/kadmin/bootstrap_helper.rb
@@ -44,33 +44,40 @@ module Kadmin
     end
 
     # Custom label generator using bootstrap i.e. * for mendatory feilds, help icon and popover for help
-    # @param [String] label text for the view
-    # @param [String] label_for is ID of the html element for which we are displaying label.
-    # @param [Boolean] Display the red asteric to indicate mendatory field.
-    # @param [Boolean] to decide weither we need to display the help iocn or not
-    # @param [String] Title of the popover
-    # @param [String] Body text of popover
-    # @param [String] classname for the label.
-    def create_custom_label(label, label_for = '', required = false, display_help = true, title = '', message = '', label_class = 'control-label')
-      label = t(label)
+    # @param [String] key identifier for the help label / input field
+    # @param [Hash] options options for the help label
+    def create_custom_label(key, options = {})
+      defaults = {
+        label_for: "#{key}",
+        required: false,
+        display_help: true,
+        title: ".#{key}.title",
+        message: ".#{key}.message",
+        label_class: 'control-label',
+        data_placement: 'top'
+      }
+      options = defaults.merge(options)
+
+      label = t(".#{key}.label")
       label = label.html_safe
 
-      require_html = required ? '<span class="required-field"><span>' : ''
+      require_html = options[:required] ? '<span class="required-field"><span>' : ''
       icon_html = "<i class='fa fa-question-circle' style='color:green'></i>"
 
       display_message = ''
-      if display_help
-        message = t(message)
+      if options[:display_help]
+        message = t(options[:message])
         message = message.gsub('"', '\"')
         message = message.gsub("'", "\'")
         html_message = message.html_safe
 
-        title = t(title)
+        title = t(options[:title])
         title = title.html_safe
-        display_message = "<span data-toggle='popover' title='#{title}' data-placement='top' data-trigger='hover' data-content='#{html_message}'>#{icon_html}</span>"
+
+        display_message = "<span data-toggle='popover' title='#{title}' data-placement='#{options[:data_placement]}' data-trigger='hover' data-content='#{html_message}'>#{icon_html}</span>"
       end
 
-      return "<label for='#{label_for}' class='#{label_class}'' >#{require_html}#{label} #{display_message}</label>".html_safe
+      return "<label for='#{options[:label_for]}' class='#{options[:label_class]}'>#{require_html}#{label} #{display_message}</label>".html_safe
     end
   end
 end
